d0583d7586744ecc42482528a80607ffebffe17b
Anatoliy <Anatoliy.Velizhanin@gmail.com>
2021-04-24 18:27:18 +0500

    160 characters in a line, some exclusions for pylint and flake8

--- server/models/user.py
+++ server/models/user.py
@@ -8,5 +8,5 @@ class User(BaseModel):
     firstName: str
     middleName: str
     lastName: str
-    email: str = Query(..., regex="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")  # noqa: W605
+    email: str = Query(..., regex="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$")  # noqa: W605 # pylint: disable=W1401
     role: Role
